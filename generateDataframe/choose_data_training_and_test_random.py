bb5b32fe297251f494997f96c3dba6b90a9c8a10
Yuri Kelvin <yuri.silva@ccc.ufcg.edu.br>
2018-11-28 21:33:40 -0300

    Generate the dataframes of data training and data test

---
+++ generateDataframe/choose_data_training_and_test_random.py
@@ -0,0 +1,43 @@
+import pandas as pd # Require the pandas module installed
+import numpy as np
+
+def diff(first, second):
+        second = set(second)
+        return [item for item in first if item not in second]
+
+np.random.seed(10)
+
+PERCENT_OF_DATA_TRAINING = 0.8
+
+print 'Reading the csv'
+
+df = pd.read_csv("../dataframes/true_car_listings.csv")
+
+print 'Read completed...'
+print 'Deleting the Vin unused column'
+
+dfWithoutVin = df.drop('Vin', 1)
+sizeOfDf = len(dfWithoutVin.index)
+
+sizeOfDfTraining = int(round(PERCENT_OF_DATA_TRAINING * sizeOfDf, 0))
+
+print 'Sucessful remove the Vin column and calculate the number that represents 80% percent of data training of whole data'
+print repr(sizeOfDfTraining) + ' represents (80%) of ' + repr(sizeOfDf)
+
+indexesWholeData = dfWithoutVin.index.tolist()
+indexesDataTraining = np.random.choice(dfWithoutVin.index, sizeOfDfTraining, replace=False)
+
+print "Indexes of data training completed."
+
+indexesDataTest = diff(indexesWholeData, indexesDataTraining) # The indexes of Data Test represents 20% of the whole of data
+
+dataTraining = dfWithoutVin.drop(indexesDataTraining)
+dataTest = dfWithoutVin.drop(indexesDataTest)
+
+print 'Sucessful extract the data training and data test from whole data'
+
+print 'Generating the output csv'
+
+dataTraining.to_csv('../dataframes/true_car_listings_data_training.csv')
+dataTest.to_csv('../dataframes/true_car_listings_data_test.csv')
+
